25158510a3fda1fc79546af9d3c5113676d48794
Mazen Mohamed Nayef <mazennayef2002@gmail.com>
2026-04-15 23:15:03 +0200

    feat: Refactor AuthRemoteDataSourceImpl to improve dependency injection and add CancelToken support for registration

--- lib/features/Auth/data/datasources/remote/auth_remote_impl.dart
+++ lib/features/Auth/data/datasources/remote/auth_remote_impl.dart
@@ -2,25 +2,37 @@ import 'package:dio/dio.dart';
 import 'package:viora_app/core/api/api_consumer.dart';
 import 'package:viora_app/core/api/end_points.dart';
 import 'package:viora_app/core/params/user_parameters.dart';
-import 'package:viora_app/features/Auth/data/datasources/remote/auth_remote.dart';
-import 'package:viora_app/features/Auth/data/models/user_model.dart';
+import 'package:viora_app/features/auth/data/datasources/local/auth_local.dart';
+import 'package:viora_app/features/auth/data/datasources/remote/auth_remote.dart';
+import 'package:viora_app/features/auth/data/models/user_model.dart';
 
 class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
-  final ApiConsumer apiConsumer;
+  final ApiConsumer _apiConsumer;
+  final AuthLocalDataSource _authLocalDataSource;
 
-  AuthRemoteDataSourceImpl(this.apiConsumer);
+  AuthRemoteDataSourceImpl(this._apiConsumer, this._authLocalDataSource);
 
   @override
   Future<UserModel> login(LoginParameters params) async {
-    final response = await apiConsumer.post(
+    final response = await _apiConsumer.post(
       EndPoints.loginUrl,
       data: {'email': params.email, 'password': params.password},
     );
+
+    // Extract the token from the response and save it to local storage
+    final token = response['token'];
+    if (token != null) {
+      await _authLocalDataSource.saveUserToken(token);
+    }
+
     return UserModel.fromJson(response);
   }
 
   @override
-  Future<UserModel> register(RegisterParameters params) async {
+  Future<UserModel> register(
+    RegisterParameters params, {
+    CancelToken? cancelToken,
+  }) async {
     final data = <String, dynamic>{
       'email': params.email,
       'password': params.password,
@@ -43,10 +55,11 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
       );
     }
 
-    final response = await apiConsumer.post(
+    final response = await _apiConsumer.post(
       EndPoints.registerUrl,
       data: data,
       isFormData: hasProfilePicture,
+      cancelToken: cancelToken,
     );
     return UserModel.fromJson(response);
   }
